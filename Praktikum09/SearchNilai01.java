b6ab547a457c5ff511f9c48acaeac8c3cf8d4acd
Adi Luhung <luhungadi571@gmail.com>
2024-11-01 10:15:58 +0700

    modifikasi

--- Praktikum09/SearchNilai01.java
+++ Praktikum09/SearchNilai01.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
 public class SearchNilai01 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 78;
-        int hasil = 0;
+        System.out.print("Masukkan jumlah mahasiswa yang akan diinput : ");
+        int jumlah = sc.nextInt();
+        int [] nilaiMhs = new int[jumlah];
+        int hasil = 0, key;
 
-        for (int i = 0; i < arrNilai.length; i++) {
-            if (key == arrNilai[i]) {
-                hasil = i;
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yang dicari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (key == nilaiMhs[i]) {
+                hasil = i + 1;
                 break;
             }
+       
         }
-        System.out.println("Nilai " + key + " ditemukan pada indeks ke-" + hasil);
+        if (hasil == 0) {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+            
+        }else {
+            System.out.println();
+            System.out.println("Nilai " + key + " merupakan nilai mahasiswa ke-" + hasil);
+            System.out.println();
+        }
+
     }
 }
