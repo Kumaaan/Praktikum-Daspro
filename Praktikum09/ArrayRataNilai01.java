61a5f6e7dece53f0a1fc121993c787a695ca485a
Adi Luhung <luhungadi571@gmail.com>
2024-11-01 09:29:25 +0700

    modifikasi

--- Praktikum09/ArrayRataNilai01.java
+++ Praktikum09/ArrayRataNilai01.java
@@ -3,18 +3,29 @@ public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-        int [] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlah = sc.nextInt();
+        int [] nilaiMhs = new int[jumlah];
+        double rata1, rata2, mhsLulus = 0, mhsTdkLulus = 0;
+        double totalNilaiLulus = 0, totalNilaiTdkLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.err.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
-             nilaiMhs[i] = sc.nextInt();
+            nilaiMhs[i] = sc.nextInt();
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70){
+                mhsLulus++;
+                totalNilaiLulus += nilaiMhs[i];
+            }else {
+                mhsTdkLulus++;
+                totalNilaiTdkLulus += nilaiMhs[i];
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nila = "+rata2);
+        rata1 = totalNilaiLulus/mhsLulus;
+        System.out.println("Rata-rata nila mahasiswa lulus = "+rata1);
+        rata2 = totalNilaiTdkLulus/mhsTdkLulus;
+        System.out.println("Rata-rata nila mahasiswa tidak lulus = "+rata2);
+        sc.close();
     }
 }
